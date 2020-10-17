35da5eee4d045ebd41e4e0fceac938a960f8e182
Lee-Roy King <leeroywking@gmail.com>
2020-10-17 16:05:47 -0700

    got rid of stupid extra button that was annoying Natalie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm';
 import './App.css';
 import SalesData from "./components/SalesData.js"
 
@@ -54,12 +53,11 @@ class App extends Component {
           displayed_form: '',
           username: data.username
         });
+        this.handle_signup()
       });
   };
 
-  handle_signup = (e, data) => {
-    // this needs renamed
-    e.preventDefault();
+  handle_signup = () => {
     fetch(sales_endpoint, {
       method: 'GET',
       headers: {
@@ -87,27 +85,12 @@ class App extends Component {
   };
 
   render() {
-    // let form;
-    // switch (this.state.displayed_form) {
-    //   case 'login':
-    //     form = <LoginForm handle_login={this.handle_login} />;
-    //     break;
-    //   case 'signup':
-    //     form = <SignupForm handle_signup={this.handle_signup} />;
-    //     break;
-    //   default:
-    //     form = null;
-    // }
+
 
     return (
       <div className="App">
-        {/* <Nav
-          logged_in={this.state.logged_in}
-          display_form={this.display_form}
-          handle_logout={this.handle_logout}
-        /> */}
+
         <LoginForm handle_login={this.handle_login} />
-        <SignupForm handle_signup={this.handle_signup} />
         <h3>
           {this.state.logged_in
             ? `Hello, ${this.state.username}`
